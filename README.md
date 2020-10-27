1a33a804fe603eb847920c48536172df29c68a11
jnasir215 <jnasir247@gmail.com>
2020-10-27 20:58:09 +0000

    Test Comment

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/code-differently/JavaWeek01Workshop)
+
 # Today's Lab
 
 Look in the folders!!!
--- src/main/java/com/codedifferently/part01/Greetings.java
+++ src/main/java/com/codedifferently/part01/Greetings.java
@@ -1,7 +1,19 @@
 package com.codedifferently.part01;
 
+import java.io.Console;
+
 public class Greetings {
     public static void main(String[] args) {
 
-    }
+        Console console = System.console();			//Defines console to elicit processes from the class Console
+        String name = console.readLine("What is your name? ");  //Assigns user input to the String variable "name"
+		if((name.equalsIgnoreCase("Hakim")) ||
+		   (name.equalsIgnoreCase("Rasheeda"))) {
+			console.printf("Hello %s \n", name);		//Greets the user if their  name is Hakim or Rasheeda
+			System.exit(0);					//Exits the program
+		}
+		else console.printf("We are looking to greet either Hakim or Rasheeda. Goodbye! \n");
+		System.exit(0);		//If their name is not Hakim or Rasheeda, lets the user know and Exits the program
+	}
 }
+
